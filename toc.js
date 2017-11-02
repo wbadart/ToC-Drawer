aa78678ab3c6f56cb7e3c6eed5c01351ade32766
Will Badart <wbadart@nd.edu>
2017-11-02 19:26:27 -0400

    CSS fixes, title_predicate update Tab now fully connects to toc-main. Changed title_predicate to title_selector; CSS selector for children of .toc-articles which should have their textContents as titles. README update to follow.

--- toc.js
+++ toc.js
@@ -10,9 +10,25 @@
 var TOC_STATES = {shown: 0, hidden: 1};
 
 
-function ToC(config) {
-    config = config || {};
-    this.state = config.start_state || TOC_STATES.shown;
+function ToC(usr_config) {
+
+    //==========================
+    // Apply user configuration
+    //==========================
+
+    var config = Object.assign({
+        // Default settings
+        start_state: TOC_STATES.shown,
+        header: 'Table of Contents',
+        title_selector: 'h2',
+    }, usr_config);
+
+    this.state = config.start_state;
+
+
+    //====================
+    // Create DOM outline
+    //====================
 
     this.handle =
     this.container = document.createElement('aside');
@@ -23,31 +39,44 @@ function ToC(config) {
     this.tab       = document.createElement('div');
     this.icon      = document.createElement('i');
 
+    this.container.appendChild(this.main);
+    // this.container.appendChild(this.spacer);
+    this.container.appendChild(this.tab);
+    this.main.appendChild(this.title);
+    this.main.appendChild(this.ul);
+    this.tab.appendChild(this.icon);
+
+
+    //==============================
+    // Apply classes, populate text
+    //==============================
+
     this.container.classList.add('toc-box');
     this.main.classList.add('toc-main');
     this.spacer.classList.add('toc-spacer');
     this.tab.classList.add('toc-tab');
     this.icon.classList.add('fa', 'fa-list');
 
-    this.container.appendChild(this.main);
-    this.container.appendChild(this.spacer);
-    this.container.appendChild(this.tab);
-    this.main.appendChild(this.title);
-    this.main.appendChild(this.ul);
-    this.tab.appendChild(this.icon);
-    this.title.textContent = config.header || 'Table of Contents';
+    this.title.textContent = config.header;
     this.spacer.innerHTML = '&nbsp;';
 
     var text_getter = get('textContent');
-    this.articles = Array.from(document.getElementsByClassName('toc-article'));
+    this.articles = Array.from(
+        document.getElementsByClassName('toc-article'));
     this.articles.forEach((function(article, i, articles) {
-        flattree(article)
-            .filter(config.title_predicate || default_title_predicate)
+        var this_class = 'toc-article' + i;
+        article.classList.add(this_class);
+
+        Array.from(document.querySelectorAll(
+                '.' + this_class + ' ' + config.title_selector))
             .map(add_ids.bind(null, text_getter))
             .map(text_getter)
             .map(li)
-            .concat(!last(i, articles) ? [document.createElement('hr')] : [])
+            .concat(!last(i, articles)
+                ? [document.createElement('hr')]
+                : [])
             .forEach(this.ul.appendChild.bind(this.ul));
+
     }).bind(this));
 
     this.container.style.left = this.main.clientWidth;
@@ -58,9 +87,14 @@ function ToC(config) {
         this.state = this.state === TOC_STATES.shown
             ? TOC_STATES.hidden
             : TOC_STATES.shown;
-        this.container.style.left = (this.state === TOC_STATES.shown
+        this.container.style.left = get_css_left(
+            this.state, this.main.clientWidth);
+    }
+
+    function get_css_left(state, box_width) {
+        return (state == TOC_STATES.shown
             ? 0
-            : -this.main.clientWidth) + 'px';
+            : -(box_width + 1)) + 'px';
     }
 
     function add_ids(text_getter, e) {
@@ -74,22 +108,17 @@ function ToC(config) {
         t.textContent = text;
         e.appendChild(t);
         e.onclick = function() {
-            document.querySelector('#' + text_to_id(text)).scrollIntoView({behavior: 'smooth'}) };
+            document.querySelector('#' + text_to_id(text))
+                .scrollIntoView({behavior: 'smooth'})
+        };
         return e;
-    }
-
-    function flattree(e) {
-        return Array.from(e.children).reduce(function(result, child) {
-            return result.concat(haschildren(child) ? flattree(child) : [child]);
-        }, []).concat([e])
-        function haschildren(e) { return e.children.length !== 0 }
     }
 
     function text_to_id(text) {
         return text
             .replace(/[']/g, '')
             .replace(/\W/g, ' ')
-            .split(/\s/)
+            .split(/\s+/)
             .map(capitalize)
             .join('');
     }
@@ -97,5 +126,4 @@ function ToC(config) {
     function capitalize(w) { return w.charAt(0).toUpperCase() + w.slice(1).toLowerCase() }
     function get(attr) { return function(obj) { return obj[attr] } }
     function last(index, arr) { return index === arr.length - 1 }
-    function default_title_predicate (e) { return e.tagName === 'H2' };
 }
