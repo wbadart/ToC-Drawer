e3da3fbdbf6033578f8b8345922c24cd4d10f0b1
Will Badart <wbadart@nd.edu>
2017-11-02 20:06:47 -0400

    Added Object.assign polyfill to distribution

---
+++ assign.polyfill.js
@@ -0,0 +1,39 @@
+/**
+ * assign.polyfill.js
+ *
+ * Adds `assign' to the Object API of older browsers. Taken from
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+ *
+ * Should be used for:
+ * Chrome<45, IE, Firefox<34, Opera<32, and Safari<9.
+ */
+
+if (typeof Object.assign != 'function') {
+  // Must be writable: true, enumerable: false, configurable: true
+  Object.defineProperty(Object, "assign", {
+    value: function assign(target, varArgs) { // .length of function is 2
+      'use strict';
+      if (target == null) { // TypeError if undefined or null
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var to = Object(target);
+
+      for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+
+        if (nextSource != null) { // Skip over if undefined or null
+          for (var nextKey in nextSource) {
+            // Avoid bugs when hasOwnProperty is shadowed
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+              to[nextKey] = nextSource[nextKey];
+            }
+          }
+        }
+      }
+      return to;
+    },
+    writable: true,
+    configurable: true
+  });
+}
